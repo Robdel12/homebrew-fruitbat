701900d69390b214e7db961560c9579c505179c4
Robert DeLuca <robertdeluca19@gmail.com>
2026-05-23 00:51:36 -0500

    🍺 Install FruitBat v0.1.0 artifact

--- Formula/fruitbat.rb
+++ Formula/fruitbat.rb
@@ -2,15 +2,13 @@ class Fruitbat < Formula
   desc "Ephemeral GitHub Actions runners for Apple Silicon Macs"
   homepage "https://github.com/Robdel12/FruitBat"
   license "MIT"
-  head "https://github.com/Robdel12/FruitBat.git", branch: "main"
-
-  depends_on "node" => :build
+  url "https://github.com/Robdel12/FruitBat/releases/download/v0.1.0/fruitbat-darwin-arm64.tar.gz"
+  sha256 "e3df0fef93c9292914b1499e466f985cdb9a38835005d0f1fb7ec4c9ecdc10d1"
 
   def install
-    system "npm", "ci"
-    system "npm", "run", "build:binary"
+    odie "FruitBat currently ships macOS arm64 binaries only" unless OS.mac? && Hardware::CPU.arm?
 
-    bin.install "dist/fruitbat"
+    bin.install "fruitbat"
   end
 
   test do
