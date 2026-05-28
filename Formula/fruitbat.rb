05450a4f0b771bea653d46f5c441282a441782ea
Robert DeLuca <robertdeluca19@gmail.com>
2026-05-27 23:18:39 -0500

    🐛 Release FruitBat v0.1.5
    Point the tap formula at the installed LaunchAgent startup fix release.

--- Formula/fruitbat.rb
+++ Formula/fruitbat.rb
@@ -2,9 +2,9 @@ class Fruitbat < Formula
   desc "Ephemeral GitHub Actions runners for Apple Silicon Macs"
   homepage "https://github.com/Robdel12/FruitBat"
   license "MIT"
-  url "https://github.com/Robdel12/FruitBat/releases/download/v0.1.4/fruitbat-darwin-arm64.tar.gz"
-  version "0.1.4"
-  sha256 "61b1422001987e03e4dc47f9c0d12c8ef716002372707dbe382c8c40a225058b"
+  url "https://github.com/Robdel12/FruitBat/releases/download/v0.1.5/fruitbat-darwin-arm64.tar.gz"
+  version "0.1.5"
+  sha256 "5b58bc5bfbcca8e1e65cf2508dd0d00d3c80b7f8e5256d4bbc2321ff979ae422"
 
   def install
     odie "FruitBat currently ships macOS arm64 binaries only" unless OS.mac? && Hardware::CPU.arm?
